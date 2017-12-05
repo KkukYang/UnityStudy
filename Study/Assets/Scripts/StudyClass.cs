46dc030d64a768904af0353bfa46b4d91c9c7bd6
KkukHyun <yangyk7364@gmail.com>
2017-12-05 16:01:41 +0900

    no message

--- Study/Assets/Scripts/StudyClass.cs
+++ Study/Assets/Scripts/StudyClass.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class StudyClass : MonoBehaviour
 {
+    public GameObject blobLightProjector;
+    public GameObject blobShadowProjector;
+    public GameObject gridProjector;
 
     void Start()
     {
@@ -12,6 +15,50 @@ public class StudyClass : MonoBehaviour
 
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            blobLightProjector.SetActive(true);
+            blobShadowProjector.SetActive(false);
+            gridProjector.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            blobLightProjector.SetActive(false);
+            blobShadowProjector.SetActive(true);
+            gridProjector.SetActive(false);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            blobLightProjector.SetActive(false);
+            blobShadowProjector.SetActive(false);
+            gridProjector.SetActive(true);
+
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            //위.
+            this.transform.localPosition += Vector3.forward.normalized * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            //왼쪽.
+            this.transform.localPosition += -Vector3.right.normalized * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //아래.
+            this.transform.localPosition += -Vector3.forward.normalized * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //오른쪽.
+            this.transform.localPosition += Vector3.right.normalized * Time.deltaTime;
+
+        }
 
     }
 }
